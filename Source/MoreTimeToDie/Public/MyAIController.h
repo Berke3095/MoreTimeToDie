933270ca5573c17d8c8c60da90e3efad9abc7647
Cemil Berke Aktepe <cemilberkeaktepe3@gmail.com>
2024-05-17 23:20:37 +0300

    AI Controller created

---
+++ Source/MoreTimeToDie/Public/MyAIController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MyAIController.generated.h"
+
+UCLASS()
+class MORETIMETODIE_API AMyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
